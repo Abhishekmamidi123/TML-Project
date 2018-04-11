6bc1c7bc2ef5f04d33411cf8c7331016ba0cf921
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-11 22:09:26 +0530

    Extract SIFT

--- 0_feature_extraction.py
+++ 0_feature_extraction.py
@@ -1,1 +1,20 @@
 # Feature extraction of images
+import cv2
+import numpy as np
+
+#directories = ['coast', 'forest', 'highway', 'inside_city', 'mountain', 'Opencountry', 'street', 'tallbuilding']
+
+#for dir in directories:
+#	filepath = 'image_data'
+	
+path = 'art294.jpg'
+image = cv2.imread(path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp, des = sift.detectAndCompute(gray,None)
+
+image = cv2.drawKeypoints(gray, kp, None)
+cv2.imwrite('x.jpg', image)
+print des
+print des.shape
