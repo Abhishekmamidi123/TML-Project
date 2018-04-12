ab31212fe955be5cb6a1598415bca82d6dc61d0b
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-12 22:24:17 +0530

    LMS - Almost Done

--- 1_Binary_SVM.py
+++ 1_Binary_SVM.py
@@ -1,16 +1,18 @@
 # Binary SVM
 import numpy as np
+np.set_printoptions(threshold=np.nan)
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
 def svm_classifer(train_images, train_labels):
-	rbf_svc = SVC(kernel='rbf', gamma=0.7, C=1)
+	rbf_svc = SVC(kernel='linear', gamma=0.1, C=1)
 	rbf_svc.fit(train_images, train_labels)
 	return rbf_svc
 
 def display_categorization_accuracy(test_labels, test_prediction):
 	categorization_accuracy = accuracy_score(test_labels, test_prediction)
-	print categorization_accuracy
+	return categorization_accuracy
 
 # Read images
 with open('0_bag_of_words_representation_of_images.txt', 'r') as file:
@@ -19,7 +21,6 @@ with open('0_bag_of_words_representation_of_images.txt', 'r') as file:
 # Read labels
 with open('0_labels.txt', 'r') as file:
 	labels = np.array(eval(file.readline()))
-
 print images.shape
 print labels.shape
 
@@ -30,16 +31,20 @@ images = images[indices]
 labels = labels[indices]
 
 # Divide into train and test sets
-dividing_point = images.shape*0.8
+print images.shape[0]
+dividing_point = int(images.shape[0]*0.8)
+print dividing_point
 X_train = images[:dividing_point]
 X_test = images[dividing_point:]
 y_train = labels[:dividing_point]
 y_test = labels[dividing_point:]
-
+# print y_train
 # Train SVM model
 svm_model = svm_classifer(X_train, y_train)
 
 # Predict
 y_pred = svm_model.predict(X_test)
+print y_pred
+print y_test
 Accuracy = display_categorization_accuracy(y_test, y_pred)
 print Accuracy
--- 2_LMS.py
+++ 2_LMS.py
@@ -1,3 +1,64 @@
 # LMS
 import numpy as np
 
+with open('0_bag_of_words_representation_of_images.txt', 'r') as file:
+	images = np.array(eval(file.readline()))
+
+# Read labels
+with open('0_labels.txt', 'r') as file:
+	labels = np.array(eval(file.readline()))
+
+# Shuffle images ad labels at a time.
+indices = np.arange(images.shape[0])
+np.random.shuffle(indices)
+images = images[indices]
+labels = labels[indices]
+
+# Normalize images
+images = images*1.0
+maximum = np.amax(images,1)
+maximum = np.reshape(maximum, (maximum.shape[0], 1))
+images = images/maximum
+
+# Divide into train and test sets
+dividing_point = int(images.shape[0]*0.8)
+print dividing_point
+X_train = images[:dividing_point]
+y_train = labels[:dividing_point]
+X_test = images[dividing_point:]
+y_test = labels[dividing_point:]
+
+mu = 0.001
+w = np.zeros((1,16))
+
+errors = []
+#for i in range(dividing_point):
+for i in range(dividing_point):
+	e_i = y_train[i] - np.matmul(w,np.reshape(X_train[i], (X_train[i].shape[0],1)))
+	e_i = np.reshape(e_i, ())
+	print e_i
+	errors.append(e_i)
+	print w
+	print X_train[i]
+	w = w + mu * int(e_i) * np.reshape(X_train[i], (1, X_train[i].shape[0]))
+	
+print w.shape
+print X_test.shape
+y_pred = np.matmul(X_test, np.transpose(w))
+print y_pred.shape
+print type(y_pred)
+
+'''
+function [yd, mse] = lmsFun(u, d, M, N)  
+    mu = 0.001;
+    w = zeros(1,M);
+    for i = 1:N
+        e(i) = d(i) - w * u(:,i);
+        w = w + mu * e(i) * (u(:,i)');
+        mse(i) = abs(e(i))^2;
+    end
+    yd=(w * u);
+    
+end
+'''
+
