be835a343f36f99539e20cb89a3e66f75d965088
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-04-12 22:42:27 +0530

    Normalize images

--- 1_Binary_SVM.py
+++ 1_Binary_SVM.py
@@ -21,8 +21,6 @@ with open('0_bag_of_words_representation_of_images.txt', 'r') as file:
 # Read labels
 with open('0_labels.txt', 'r') as file:
 	labels = np.array(eval(file.readline()))
-print images.shape
-print labels.shape
 
 # Shuffle images ad labels at a time.
 indices = np.arange(images.shape[0])
@@ -37,20 +35,18 @@ maximum = np.reshape(maximum, (maximum.shape[0], 1))
 images = images/maximum
 
 # Divide into train and test sets
-print images.shape[0]
 dividing_point = int(images.shape[0]*0.8)
-print dividing_point
 X_train = images[:dividing_point]
 X_test = images[dividing_point:]
 y_train = labels[:dividing_point]
 y_test = labels[dividing_point:]
-# print y_train
+
 # Train SVM model
 svm_model = svm_classifer(X_train, y_train)
 
 # Predict
 y_pred = svm_model.predict(X_test)
-print y_pred
-print y_test
 Accuracy = display_categorization_accuracy(y_test, y_pred)
 print Accuracy
+
+# 60.22
