7ff186b8d272fc55e6dd6d2519d87d7c0f0cb531
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-05-01 22:29:24 +0530

    MEE - Starter code

--- 5_MEE.py
+++ 5_MEE.py
@@ -1,3 +1,35 @@
 # MEE
 
+import numpy as np
+np.set_printoptions(threshold=np.nan)
+from sklearn.metrics import accuracy_score
 
+def display_categorization_accuracy(test_labels, test_prediction):
+	categorization_accuracy = accuracy_score(test_labels, test_prediction)
+	return categorization_accuracy
+
+with open('0_bag_of_words_representation_of_images.txt', 'r') as file:
+	images = np.array(eval(file.readline()))
+
+# Read labels
+with open('0_labels.txt', 'r') as file:
+	labels = np.array(eval(file.readline()))
+
+# Shuffle images ad labels at a time.
+indices = np.arange(images.shape[0])
+np.random.shuffle(indices)
+images = images[indices]
+labels = labels[indices] + 1
+
+# Normalize images
+images = images*1.0
+maximum = np.amax(images,1)
+maximum = np.reshape(maximum, (maximum.shape[0], 1))
+images = images/maximum
+
+# Divide into train and test sets
+dividing_point = int(images.shape[0]*0.8)
+X_train = images[:dividing_point]
+y_train = labels[:dividing_point]
+X_test = images[dividing_point:]
+y_test = labels[dividing_point:]
