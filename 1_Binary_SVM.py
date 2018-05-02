022d9695114050de31db2a9c2004fcfee4f7d370
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-05-02 11:27:56 +0530

    ROC Curve

--- 1_Binary_SVM.py
+++ 1_Binary_SVM.py
@@ -3,7 +3,49 @@ import numpy as np
 np.set_printoptions(threshold=np.nan)
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
+import scikitplot as skplt
+import itertools
+from itertools import cycle
+from scipy import interp
+from keras.utils import np_utils
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 def svm_classifer(train_images, train_labels):
 	rbf_svc = SVC(kernel='linear', gamma=0.1, C=1)
@@ -49,4 +91,84 @@ y_pred = svm_model.predict(X_test)
 Accuracy = display_categorization_accuracy(y_test, y_pred)
 print Accuracy
 
+# Confusion matrix
+class_names = ['coast', 'forest', 'highway', 'inside_city', 'mountain', 'Opencountry', 'street', 'tallbuilding']
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix, without normalization')
+plt.show()
+
+# ROC Curve
+
+# Compute ROC curve and ROC area for each class
+n_classes = 8
+y_pred = np_utils.to_categorical(y_pred)
+y_test = np_utils.to_categorical(y_test)
+y_pred = y_pred[:,1:]
+y_test = y_test[:,1:]
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+# Plot all ROC curves
+plt.figure()
+
+lw = 2
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Some extension of Receiver operating characteristic to multi-class')
+plt.legend(loc="lower right")
+plt.show()
+
+
+'''
+y_pred = np_utils.to_categorical(y_pred)
+print type(y_pred)	
+print y_test.shape
+print y_pred.shape
+skplt.metrics.plot_roc_curve(y_test, y_pred)
+plt.show()
+'''
+'''
+y_pred = np_utils.to_categorical(y_pred)
+y_test = np_utils.to_categorical(y_test)
+y_pred = y_pred[:,1:][:,0]
+y_test = y_test[:,1:][:,0]
+print y_pred.shape
+fpr, tpr, _ = roc_curve(y_test, y_pred)
+auc = roc_auc_score(y_test, y_pred)
+print y_test
+plt.figure()
+plt.plot(fpr,tpr,label="auc="+str(auc))
+plt.legend(loc=4)
+plt.show()
+'''
 # 60.22
